76a6f49e6811685d53476b3867256b362050c79c
Henrik Nyh <henrik@nyh.se>
2015-09-06 18:29:56 +0200

    Move code into View

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,11 +2,6 @@ defmodule QED.PageController do
   use QED.Web, :controller
 
   def index(conn, params) do
-    code = params["code"]
-    quoted = inspect Code.string_to_quoted!(code)
-
-    render conn, "index.html",
-      code: code,
-      quoted: quoted
+    render conn, "index.html", code: params["code"]
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule QED.PageView do
   use QED.Web, :view
+
+  def quoted(code) do
+    inspect Code.string_to_quoted!(code)
+  end
 end
