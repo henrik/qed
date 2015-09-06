825bbc41b6d0a92c8fd218710309f52294b90ac0
Henrik Nyh <henrik@nyh.se>
2015-09-06 20:27:47 +0200

    Comment with quoted expression reference

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -3,6 +3,7 @@ defmodule QED.PageView do
 
   # FIXME: Make HTML valid. We're mixing spans and divs willy nilly.
   # TODO: Where does this belong? Make sure Phoenix reloads the page when it changes.
+  # Reference: http://elixir-lang.org/getting-started/meta/quote-and-unquote.html
   defmodule Visualizer do
     def html(code, include_meta) do
       quoted = Code.string_to_quoted!(code)
