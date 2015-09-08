4e18869ee6929f9708fd7e412d5a54fa9574bb5f
Henrik Nyh <henrik@nyh.se>
2015-09-09 00:28:47 +0200

    Slimify

--- config/dev.exs
+++ config/dev.exs
@@ -20,7 +20,7 @@ config :qed, QED.Endpoint,
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|slim)$}
     ]
   ]
 
