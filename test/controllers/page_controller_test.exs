b6a6928485f0f60f6dc8d608f8e9d7a9ddfcb36c
Henrik Nyh <henrik@nyh.se>
2015-09-08 20:38:57 +0200

    More controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -5,4 +5,15 @@ defmodule QED.PageControllerTest do
     conn = get conn(), "/"
     assert html_response(conn, 200) =~ "Quoted Expression Demonstrator"
   end
+
+  test "GET / with code and meta flag" do
+    conn = get conn(), "/?code=1-2&meta=true"
+    response = html_response(conn, 200)
+
+    # Code
+    assert response =~ "1-2"
+
+    # Quoted
+    assert response =~ "[1, 2]"
+  end
 end
