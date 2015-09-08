a2d098f5a8efa994300dffc90845a964c120179b
Henrik Nyh <henrik@nyh.se>
2015-09-08 20:44:59 +0200

    Some PageViewTest-ing

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,13 @@
 defmodule QED.PageViewTest do
   use QED.ConnCase, async: true
+
+  test "quoted" do
+    assert QED.PageView.quoted("1+2") == "{:+, [line: 1], [1, 2]}"
+  end
+
+  # Just testing integration. Details can be tested inside Visualizer.
+  test "visualization" do
+    {:safe, html} = QED.PageView.visualization("1+2", true)
+    assert String.contains? html, ":+"
+  end
 end
