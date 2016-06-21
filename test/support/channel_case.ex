ab65f1ea0370e3c5a60e4dc028717e0e454008e5
Henrik Nyh <henrik@nyh.se>
2016-06-21 15:57:07 +0100

    Fix 'unused var' warnings in Elixir 1.3

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -26,7 +26,7 @@ defmodule QED.ChannelCase do
     end
   end
 
-  setup tags do
+  setup _tags do
 
     :ok
   end
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -27,7 +27,7 @@ defmodule QED.ConnCase do
     end
   end
 
-  setup tags do
+  setup _tags do
 
     :ok
   end
